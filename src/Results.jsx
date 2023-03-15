015f58e80cea36601fc2f4b8a6e17eb2b18de4d3
MaxManthey <maximilian.manthey@arconsis.com>
2023-03-15 09:58:46 +0100

    added pet search

---
+++ src/Results.jsx
@@ -0,0 +1,24 @@
+import Pet from "./Pet";
+
+const Results = ({ pets }) => {
+  return (
+    <div className="search">
+      {!pets.length ? (
+        <h1>No Pets Found</h1>
+      ) : (
+        pets.map((pet) => (
+          <Pet
+            name={pet.name}
+            animal={pet.animal}
+            breed={pet.breed}
+            location={`${pet.city}, ${pet.state}`}
+            images={pet.images}
+            key={pet.id}
+          />
+        ))
+      )}
+    </div>
+  );
+};
+
+export default Results;
